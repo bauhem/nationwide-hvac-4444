7f07e128ea5b6cf491d670126ab77f3674668cce
Jean-Philippe Boily, ing <jpboily@logicielsjpb.com>
2019-03-04 15:04:12 -0500

    Sends form data to Netlify

--- source/javascripts/components/UserInfo.js
+++ source/javascripts/components/UserInfo.js
@@ -4,10 +4,18 @@ import {getZone} from './UnitHelpers';
 
 const zipData = require('../../../data/zip_codes.json');
 
+const encode = (data) => {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+}
+
 class UserInfo extends React.Component {
   constructor(props) {
     super(props);
     this.name = React.createRef();
+    this.email = React.createRef();
+    this.phone = React.createRef();
     this.zip = React.createRef();
 
     this.handleClick = this.handleClick.bind(this);
@@ -22,15 +30,30 @@ class UserInfo extends React.Component {
 
     // If in no zones, redirect to call us
     if (this.zip.current.validity.valid) {
+      let state = {
+        user_name: this.name.current.value,
+        user_email: this.email.current.value,
+        user_phone: this.phone.current.value,
+        zip_code: zip_code
+      };
+
       let zone = getZone(zip_code);
+      state['zone_num'] = zone;
 
       if (zone === null) {
         // TODO - Add message why we got to contact us
         return this.props.transition({type: 'INVALID_ZIP'});
       }
 
-      // TODO - Send data to netlify
-      return this.props.saveAndContinue({zip_code: zip_code, zone_num: zone});
+      fetch("/", {
+        method: "POST",
+        headers: {"Content-Type": "application/x-www-form-urlencoded"},
+        body: encode({"form-name": "wf-form-price-form", ...state})
+      })
+        .then(() => console.log("Netlify: Lead added!"))
+        .catch(error => alert(error));
+
+      return this.props.saveAndContinue(state);
     }
 
     this.zip.current.reportValidity();
@@ -41,20 +64,25 @@ class UserInfo extends React.Component {
       <>
         <div className="div-heading-slide">
           <h3 className="titre-big">
-            Send us your name, email address, phone number and zip code to get your quotes
+            Send us your name, email address, phone number and zip code to get
+            your quotes
           </h3>
         </div>
         <div className="div-flex-h">
-          <input type="text"className="zipcode w-input" maxLength="256"
+          <input type="text" className="zipcode w-input" maxLength="256"
                  name="name" placeholder="Your name"
-                 id="name" required={true}/>
-          <input type="email"className="zipcode w-input" maxLength="256"
+                 id="name" required={true}
+                 ref={this.name} defaultValue={this.context.user_name || ''}/>
+          <input type="email" className="zipcode w-input" maxLength="256"
                  name="email" placeholder="Your email address"
-                 id="email" required={true}/>
-          <input type="phone"className="zipcode w-input" maxLength="256"
+                 id="email" required={true}
+                 ref={this.email} defaultValue={this.context.user_email || ''}/>
+          <input type="phone" className="zipcode w-input" maxLength="256"
                  name="phone" placeholder="Your phone number"
-                 id="name" required={true}/>
-          <input type="text" pattern="\d{5}" className="zipcode w-input" maxLength="5"
+                 id="name" required={true}
+                 ref={this.phone} defaultValue={this.context.user_phone || ''}/>
+          <input type="text" pattern="\d{5}" className="zipcode w-input"
+                 maxLength="5"
                  name="Zipcode" placeholder="Your zip code"
                  id="Zipcode" required={true}
                  ref={this.zip} defaultValue={this.context.zip_code || ''}/>
