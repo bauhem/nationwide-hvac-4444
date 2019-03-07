f2b1ec37f42e8bc2e12a3bf6b73d153dde29065c
Jean-Philippe Boily, ing <jpboily@logicielsjpb.com>
2019-03-07 17:07:43 -0500

    Saves selected filters in sessionsStorage Parses filters on load and force a filtering Toggles checkbox of selected filters

--- source/javascripts/mixitup-helpers.js
+++ source/javascripts/mixitup-helpers.js
@@ -1,3 +1,7 @@
+const FILTERS_STORAGE_KEY = 'mixitup_filters';
+const FILTER_REGEX = /(\.{1}[a-zA-Z0-9\-]+)/g;
+var mixer;
+
 function handleMixClick(state, futureState) {
   if (this.classList.contains('filter_button')) {
     toggleCB(this);
@@ -33,3 +37,98 @@ function disableAllFilters(el) {
     }
   });
 }
+
+function saveFilters(filters = {}) {
+  sessionStorage.setItem(FILTERS_STORAGE_KEY, JSON.stringify(filters))
+}
+
+function loadFilters() {
+  return JSON.parse(sessionStorage.getItem(FILTERS_STORAGE_KEY));
+}
+
+function getFilterGroups() {
+  let filter_groups = {};
+
+  jQuery('.div-search-dropdown .dropdown').each(function() {
+    filter_groups[jQuery(this).attr('data-filter-group')] = {};
+  });
+
+  return filter_groups;
+}
+
+function findFilterGroup(filter) {
+  let el = jQuery(`*[data-toggle="${filter}"]`);
+  return el.closest('.dropdown').attr('data-filter-group');
+}
+
+function getFiltersByGroup(filters) {
+  let filter_groups = getFilterGroups();
+  let groups_by_index = [];
+
+  filters.split(',').forEach((filter_group) => {
+    let filter_matches = filter_group.match(FILTER_REGEX);
+
+    // Find which group is at each index of the matches since we always have the
+    // same order of groups in a filter string
+    if (groups_by_index.length === 0) {
+      filter_matches.forEach((filter) => {
+        let group = findFilterGroup(filter);
+        groups_by_index.push(group);
+      })
+    }
+
+    // Push each match to the proper filter group array using the
+    // groups_by_index array
+    for (let i in filter_matches) {
+      filter_groups[groups_by_index[i]][filter_matches[i]] = true;
+    }
+  });
+
+  return filter_groups;
+}
+
+jQuery(document).ready(function ($) {
+  var containerEl = document.querySelector('.container');
+
+  if (containerEl) {
+    let filters = loadFilters();
+
+    mixer = mixitup(containerEl, {
+      pagination: {
+        limit: 12
+      },
+      multifilter: {
+        enable: true
+      },
+      animation: {
+        enable: false
+      },
+      callbacks: {
+        onMixClick: handleMixClick,
+        onMixEnd: (state) => {
+          saveFilters(state.activeFilter.selector);
+          scrollToContainer('.breadcrumbs');
+        }
+      }
+    });
+
+    if (filters !== undefined && filters !== null && filters !== '' && filters !== '.mix') {
+      let filtersGroup = getFiltersByGroup(filters);
+
+      for (let key in filtersGroup) {
+        let selectors = Object.keys(filtersGroup[key]);
+        mixer.setFilterGroupSelectors(key, selectors);
+        selectors.forEach((selector) => {
+          let el = jQuery(`*[data-toggle="${selector}"]`)[0];
+          if (el === undefined) {
+            return;
+          }
+          toggleCB(el);
+        });
+      }
+
+      mixer.parseFilterGroups();
+    }
+  }
+});
+
