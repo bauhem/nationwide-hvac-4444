341a3efbc9cfd774f9004e3125670cf2ac304669
Jean-Philippe Boily, ing <jpboily@logicielsjpb.com>
2019-04-10 09:13:44 -0400

    Updated sync script to be more system independent.

--- sync-airtable.sh
+++ sync-airtable.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
+export PATH=/usr/local/bin:$PATH
 export $(grep -v '^#' "$DIR"/.env | xargs)
 node "$DIR"/airtable-cli.js
